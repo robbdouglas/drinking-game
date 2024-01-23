f42cc9c75ac241c365533669083d6c0a464a1702
robbdouglas <robert.nimmrich@icloud.com>
2024-01-23 10:23:59 +0100

    add routing to player-settings

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 
 import "./styles/App.css";
 import LandingPage from "./components/LandingPage";
+import PlayerSettings from "./components/PlayerSettings";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Error404 from "./components/Error404";
 
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/player-settings" element={<PlayerSettings />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </Router>
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,8 +1,19 @@
+// LandingPage.jsx
+
+import { Link, useNavigate } from "react-router-dom";
+
 function LandingPage() {
+  const navigate = useNavigate();
+
+  const handleStartButtonClick = () => {
+    // Hier kannst du zusätzliche Logik hinzufügen, bevor zur PlayerSettings-Seite navigiert wird
+    navigate("/player-settings");
+  };
+
   return (
     <div>
       <h1>Schluck mit lustig</h1>
-      <button>Start</button>
+      <button onClick={handleStartButtonClick}>Start</button>
       <h3>Regeln:</h3>
       <ul style={{ listStyleType: "none", padding: 0 }}>
         <li>...</li>
@@ -13,4 +24,5 @@ function LandingPage() {
     </div>
   );
 }
+
 export default LandingPage;
