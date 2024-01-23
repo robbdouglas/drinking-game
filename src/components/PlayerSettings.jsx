f45136520ae3a8b4aaf29fa512378759014f0870
robbdouglas <robert.nimmrich@icloud.com>
2024-01-23 11:24:30 +0100

    edit PlayerSettings.jsx for input logic

--- src/components/PlayerSettings.jsx
+++ src/components/PlayerSettings.jsx
@@ -1,10 +1,14 @@
 // PlayerSettings.jsx
 
+import React, { useState } from "react";
 import "../styles/PlayerSettings.css";
 import { useNavigate } from "react-router-dom";
 
 function PlayerSettings() {
   const navigate = useNavigate();
+  const [drinkerInputs, setDrinkerInputs] = useState([]);
+  const [nonDrinkerInputs, setNonDrinkerInputs] = useState([]);
+  const [maxPlayersReached, setMaxPlayersReached] = useState(false);
 
   const handleBackToHomeButtonClick = () => {
     navigate("/");
@@ -13,6 +17,66 @@ function PlayerSettings() {
   const handleStartGameButtonClick = () => {
     // Hier kannst du zusätzliche Logik hinzufügen, bevor zur PlayerSettings-Seite navigiert wird
     navigate("/categories");
+  };
+
+  const handleAddDrinkerButtonClick = () => {
+    if (drinkerInputs.length + nonDrinkerInputs.length < 8) {
+      setDrinkerInputs((prevInputs) => [
+        ...prevInputs,
+        {
+          id: prevInputs.length,
+          input: (
+            <input
+              key={prevInputs.length}
+              type="text"
+              placeholder="Gib deinen Namen ein..."
+            />
+          ),
+        },
+      ]);
+    } else {
+      setMaxPlayersReached(true);
+    }
+  };
+
+  const handleAddNonDrinkerButtonClick = () => {
+    if (drinkerInputs.length + nonDrinkerInputs.length < 8) {
+      setNonDrinkerInputs((prevInputs) => [
+        ...prevInputs,
+        {
+          id: prevInputs.length,
+          input: (
+            <input
+              key={prevInputs.length}
+              type="text"
+              placeholder="Gib deinen Namen ein..."
+            />
+          ),
+        },
+      ]);
+    } else {
+      setMaxPlayersReached(true);
+    }
+  };
+
+  const handleRemoveInput = (inputType, id) => {
+    if (inputType === "drinker") {
+      setDrinkerInputs((prevInputs) =>
+        prevInputs.filter((input) => input.id !== id)
+      );
+    } else if (inputType === "nonDrinker") {
+      setNonDrinkerInputs((prevInputs) =>
+        prevInputs.filter((input) => input.id !== id)
+      );
+    }
+
+    // Überprüfe, ob die Anzahl der Inputfelder genau 8 beträgt
+    if (
+      maxPlayersReached &&
+      drinkerInputs.length + nonDrinkerInputs.length === 8
+    ) {
+      setMaxPlayersReached(false);
+    }
   };
 
   return (
@@ -28,13 +92,49 @@ function PlayerSettings() {
         </tr>
         <tr>
           <td>
-            <button>+</button>
+            <button
+              className="add-drinker-btn"
+              onClick={handleAddDrinkerButtonClick}
+            >
+              +
+            </button>
+            {drinkerInputs.map((inputObj) => (
+              <div key={inputObj.id}>
+                {inputObj.input}
+                <button
+                  onClick={() => handleRemoveInput("drinker", inputObj.id)}
+                >
+                  {" "}
+                  -{" "}
+                </button>
+              </div>
+            ))}
           </td>
           <td>
-            <button>+</button>
+            <button
+              className="add-non-drinker-driver-btn"
+              onClick={handleAddNonDrinkerButtonClick}
+            >
+              +
+            </button>
+            {nonDrinkerInputs.map((inputObj) => (
+              <div key={inputObj.id}>
+                {inputObj.input}
+                <button
+                  onClick={() => handleRemoveInput("nonDrinker", inputObj.id)}
+                >
+                  {" "}
+                  -{" "}
+                </button>
+              </div>
+            ))}
           </td>
         </tr>
       </table>
+      {maxPlayersReached &&
+        drinkerInputs.length + nonDrinkerInputs.length === 8 && (
+          <p>Die maximale Spielerzahl beträgt 8 Spieler!</p>
+        )}
       <button onClick={handleStartGameButtonClick}>SPIEL STARTEN</button>
       <button>Menü</button>
       <button className="back-to-home" onClick={handleBackToHomeButtonClick}>
