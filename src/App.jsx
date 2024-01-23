311fa99715726e1e12cf963d1f4ce94b694da335
robbdouglas <robert.nimmrich@icloud.com>
2024-01-23 10:17:07 +0100

    add basic routing, landing page and error404

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+// App.jsx
+
 import "./styles/App.css";
-import Task from "./components/Task";
+import LandingPage from "./components/LandingPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Error404 from "./components/Error404";
 
 function App() {
   return (
-    <>
-      <Task />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </Router>
   );
 }
 
