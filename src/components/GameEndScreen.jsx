55da83985e87828d38d8ee92c22655982dedb5d5
AxelH97 <hilde.axel@web.de>
2024-01-23 09:41:39 +0100

    create basic GameEndScreen

---
+++ src/components/GameEndScreen.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function GameEndScreen() {
+  return (
+    <div>
+      <h1>Congratś! Youre drunk now!</h1>
+      <button>New Game</button>
+      <button>Menü</button>
+    </div>
+  );
+}
+
+export default GameEndScreen;
