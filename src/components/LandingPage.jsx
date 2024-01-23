30ba9d815273e2edced957490cd08917af1654e6
robbdouglas <robert.nimmrich@icloud.com>
2024-01-23 15:59:47 +0100

    edit rules

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -18,14 +18,42 @@ function LandingPage() {
       <h3>Regeln:</h3>
       <ul style={{ listStyleType: "none", padding: 0 }}>
         <li>Horrido, ihr suffelwütigen Knallerbsen!</li>
-        <li>Seid ihr euch sicher, dass ihr dieses gefährliche Spiel spielen wollt, so klickt auf den "Start" Button!</li>
-        <li>Überlegt anschließend, ob ihr während des Spiels Alkohol trinken möchtet oder nicht oder ob ihr sogar der arme Fahrer seid (RIP).</li>
-        <li>Um einen neuen Spieler hinzuzufügen, klickt jeweils auf den "+" Button und tragt eure Namen ein!</li>
-        <li>Anschließend wird per Zufall ausgelost, wer von euch eine Aufgabe bewältigen muss.</li>
-        <li>Der aktive Spieler muss sich nun für eine Kategorie entscheiden.</li>
-        <li>Erfüllt dann die Anforderungen der Aufgabe und klickt auf den "Aufgabe gemeistert" Button, wenn ihr damit fertig seid.</li>
-        <li>Habt ihr keine Lust auf eine bestimmte Aufgabe, habt ihr insgesamt drei Joker, die es euch erlauben, eine Aufgabe zu überspringen.</li>
-        <li>Nach genau 30 Aufgaben ist das Spiel beendet und ihr seid vermutlich betrunkener als vorher!</li>
+        <li>
+          Seid ihr euch sicher, dass ihr dieses gefährliche Spiel spielen wollt,
+          so klickt auf den "Start" Button!
+        </li>
+        <li>
+          Überlegt anschließend, ob ihr während des Spiels Alkohol trinken
+          möchtet oder nicht oder ob ihr sogar der arme Fahrer seid (RIP).
+        </li>
+        <li>
+          Um einen neuen Spieler hinzuzufügen, klickt jeweils auf den "+" Button
+          und tragt eure Namen ein!
+        </li>
+        <li>
+          Anschließend wird per Zufall ausgelost, wer von euch eine Aufgabe
+          bewältigen muss.
+        </li>
+        <li>
+          Der aktive Spieler muss sich nun für eine Kategorie entscheiden.
+        </li>
+        <li>
+          Gut für Nicht-Trinker: Ihr dürft gern dennoch die Kategorien wählen,
+          in denen getrunken wird. Hier bekommt ihr allerdings Aufgaben, die ihr
+          lösen müsst, um Schlücke an Mitspieler zu verteilen!
+        </li>
+        <li>
+          Erfüllt dann die Anforderungen der Aufgabe und klickt auf den "Aufgabe
+          gemeistert" Button, wenn ihr damit fertig seid.
+        </li>
+        <li>
+          Habt ihr keine Lust auf eine bestimmte Aufgabe, habt ihr insgesamt
+          drei Joker, die es euch erlauben, eine Aufgabe zu überspringen.
+        </li>
+        <li>
+          Nach genau 30 Aufgaben ist das Spiel beendet und ihr seid vermutlich
+          betrunkener als vorher!
+        </li>
       </ul>
       <button className="settings-btn">Menü</button>
     </div>
